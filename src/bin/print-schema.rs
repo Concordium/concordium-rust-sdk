25421e2c0b1c6cef2665c1c5f228d0f153afd579
Kasper Dissing Bargsteen <kb@concordium.com>
2022-04-26 10:20:42 +0200

    Generate and write schemas to files

--- src/bin/print-schema.rs
+++ src/bin/print-schema.rs
@@ -1,22 +1,54 @@
-// use anyhow::Context;
-// use concordium_rust_sdk::{self, endpoints, *};
+use concordium_rust_sdk::types::{
+    hashes::TransactionHash,
+    queries::*,
+    smart_contracts::{InstanceInfo, ModuleRef},
+    *,
+};
+use id::types::{AccountAddress, ArInfo, GlobalContext, IpInfo};
+use schemars::JsonSchema;
+use std::fs;
 
-// #[tokio::main(flavor = "multi_thread")]
-// async fn main() -> anyhow::Result<()> {
-//     let mut client = endpoints::Client::connect("http://localhost:10000", "rpcadmin").await?;
+const SCHEMA_FOLDER: &str = "schemas";
 
-//     let consensus_info = client.get_consensus_status().await?;
+fn main() { generate_and_write_schemas(); }
 
-//     let res = client
-//         .get_pool_status_raw(None, &consensus_info.best_block)
-//         .await?;
-//     println!("{}", res);
-//     Ok(())
-// }
+fn generate_and_write_schemas() {
+    // Ensure the schema folder exists.
+    fs::create_dir_all(SCHEMA_FOLDER).expect("Could not create schema folder");
 
-use concordium_rust_sdk::types::{queries::*, smart_contracts::InstanceInfo, *};
-fn main() -> anyhow::Result<()> {
-    let schema = schemars::schema_for!(AccountInfo);
-    println!("{}", serde_json::to_string_pretty(&schema).unwrap());
-    Ok(())
+    write_schema_to_file::<TransactionStatusInBlock>("GetTransactionStatusInBlock");
+    write_schema_to_file::<TransactionStatus>("GetTransactionStatus");
+    write_schema_to_file::<ConsensusInfo>("GetConsensusInfo");
+    write_schema_to_file::<BlockInfo>("GetBlockInfo");
+    write_schema_to_file::<BlockSummary>("GetBlockSummary");
+    // GetBlocksAtHeight (Omitted)
+    write_schema_to_file::<Vec<AccountAddress>>("GetAccountList");
+    write_schema_to_file::<Vec<ContractAddress>>("GetInstances");
+    write_schema_to_file::<AccountInfo>("GetAccountInfo");
+    write_schema_to_file::<Vec<TransactionHash>>("GetAccountNonFinalized");
+    write_schema_to_file::<AccountNonceResponse>("NextAccountNonce");
+    write_schema_to_file::<InstanceInfo>("GetInstances");
+    // InvokeContract (Omitted)
+    write_schema_to_file::<PoolStatus>("GetPoolStatus");
+    write_schema_to_file::<Vec<BakerId>>("GetBakerList");
+    write_schema_to_file::<RewardsOverview>("GetRewardStatus");
+    write_schema_to_file::<BirkParameters>("GetBirkParameters");
+    write_schema_to_file::<Vec<ModuleRef>>("GetModuleList");
+    // GetNodeInfo..GetAncestors (Omitted)
+    write_schema_to_file::<Branch>("GetBranches");
+    // GetBannedPeers..DumpStop (Omitted)
+    write_schema_to_file::<Vec<IpInfo<wrappers::WrappedPairing>>>("GetIdentityProviders");
+    write_schema_to_file::<Vec<ArInfo<wrappers::WrappedCurve>>>("GetAnonymityRevokers");
+    write_schema_to_file::<GlobalContext<wrappers::WrappedCurve>>("GetCryptographicParameters");
+}
+
+fn write_schema_to_file<T: JsonSchema>(endpoint_name: &str) {
+    let file_name = format!("{}/{}.json", SCHEMA_FOLDER, endpoint_name);
+    println!("Writing {}", file_name);
+    let schema = schemars::schema_for!(T);
+    let contents = format!(
+        "{}",
+        serde_json::to_string_pretty(&schema).expect("Unable to pretty print JSON schema")
+    );
+    fs::write(file_name, contents).expect("Unable to write file");
 }
