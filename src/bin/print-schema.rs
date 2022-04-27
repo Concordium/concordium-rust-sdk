49c8249f2b3827573e17f92b1c680fbed2be02ef
Kasper Dissing Bargsteen <kb@concordium.com>
2022-04-27 15:10:53 +0200

    Fetch transaction statuses for testing

--- src/bin/print-schema.rs
+++ src/bin/print-schema.rs
@@ -19,8 +19,8 @@ const TEST_CASE_FOLDER: &str = "test_cases";
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> anyhow::Result<()> {
-    generate_and_write_schemas();
-    // crawl_and_save_block_summaries().await?;
+    // generate_and_write_schemas();
+    crawl_and_save_test_cases().await;
     Ok(())
 }
 
@@ -67,10 +67,12 @@ fn write_schema_to_file<T: JsonSchema>(endpoint_name: &str) {
     fs::write(file_name, contents).expect("Unable to write file");
 }
 
-async fn crawl_and_save_block_summaries() -> anyhow::Result<()> {
+async fn crawl_and_save_test_cases() -> anyhow::Result<()> {
     // Ensure folder is created for tests
-    let current_test_folder = format!("{}/block_summary", TEST_CASE_FOLDER);
-    fs::create_dir_all(&current_test_folder).expect("Could not create test folder");
+    let block_summary_folder = format!("{}/block_summary", TEST_CASE_FOLDER);
+    let transactions_folder = format!("{}/trx_status", TEST_CASE_FOLDER);
+    fs::create_dir_all(&block_summary_folder).expect("Could not create test folder");
+    fs::create_dir_all(&transactions_folder).expect("Could not create test folder");
 
     let mut client = endpoints::Client::connect("http://localhost:10000", "rpcadmin").await?;
 
@@ -83,14 +85,40 @@ async fn crawl_and_save_block_summaries() -> anyhow::Result<()> {
 
         // Get block summary and write to a file.
         let bs = client
-            .get_block_summary_raw(&cb)
+            .get_block_summary(&cb)
             .await
             .context("Could not get block summary.")?;
 
-        let file = format!("{}/{}.json", current_test_folder, cb);
+        let trxs = bs.transaction_summaries();
+
+        if trxs.len() != 0 {
+            let mut handles = Vec::with_capacity(1000);
+            for trx in trxs {
+                let th = trx.hash;
+                println!("        {}", th);
+                let cc = client.clone();
+                handles.push(tokio::spawn(async move {
+                    let mut cc = cc;
+                    (th, cc.get_transaction_status(&th).await)
+                }));
+            }
+            let x = futures::future::join_all(handles).await;
+            for res in x {
+                let (th, trx_status_res) = res?;
+                let trx_status = trx_status_res?;
+                let trx_file = format!("{}/{}.json", transactions_folder, th);
+                fs::write(
+                    trx_file,
+                    serde_json::to_string_pretty(&trx_status)
+                        .expect("Could not serialize transaction status to json"),
+                )?;
+            }
+        }
+
+        let file = format!("{}/{}.json", block_summary_folder, cb);
         fs::write(
             file,
-            serde_json::to_string_pretty(&bs).expect("Unable to pretty print JSON"),
+            serde_json::to_string_pretty(&bs).expect("Could not serialize block summary to json"),
         )?;
 
         // Find parent block hash
