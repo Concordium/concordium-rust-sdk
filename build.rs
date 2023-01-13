bf93eb207b448eca93d172892402c93c37ee6aee
Aleš Bizjak <ab@concordium.com>
2023-01-13 22:17:02 +0100

    Move grpc-api submodule to concordium-base.

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("concordium-grpc-api/concordium_p2p_rpc.proto")?;
+    tonic_build::compile_protos("concordium-base/concordium-grpc-api/concordium_p2p_rpc.proto")?;
 
     // Compile
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
-        .compile(&["concordium-grpc-api/v2/concordium/service.proto"], &[
-            "concordium-grpc-api/",
-        ])?;
+        .compile(
+            &["concordium-base/concordium-grpc-api/v2/concordium/service.proto"],
+            &["concordium-base/concordium-grpc-api/"],
+        )?;
     Ok(())
 }
