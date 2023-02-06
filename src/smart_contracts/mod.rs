32f1585a804b4e45b8acf79702a6fcac25f13408
Aleš Bizjak <ab@concordium.com>
2023-02-06 19:23:29 +0100

    Bump base after renaming wasm-chain-integration.

--- src/smart_contracts/mod.rs
+++ src/smart_contracts/mod.rs
@@ -4,7 +4,7 @@ pub use super::types::smart_contracts as types;
 /// Access to the execution engine of smart contracts.
 /// This has functionality for parsing and executing smart contracts,
 /// manipulating their state, and dry-running.
-pub use wasm_chain_integration as engine;
+pub use concordium_smart_contract_engine as engine;
 
 /// Functionality that is common to on and off-chain smart contracts.
 pub use concordium_base::contracts_common as common;
