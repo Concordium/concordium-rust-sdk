7f773a7b1f12b59dc0473036385ed4aeb61ed756
Kasper Dissing Bargsteen <kb@concordium.com>
2023-03-20 14:32:19 +0100

    Move `ContractTraceElement` and supporting types to base (#79)
    * Move `ContractTraceElement` and supporting types to base
    
    These types are also needed in the integration testing library.
    
    - Also moves:
      - `ContractEvent`
      - `InstanceUpdatedEvent`
      - `MAX_ALLOWED_INVOKE_ENERGY`
    
    * Update dependency
    
    * Update dependency
    
    * Fix typo

--- src/cis2/types.rs
+++ src/cis2/types.rs
@@ -1,22 +1,6 @@
 //! This module contains types and their implementations related to the CIS-2
 //! token standard.
 
+// All code from this module has been moved to cis2_types in base, but this is
+// still re-exported for backward compatibility.
 pub use concordium_base::cis2_types::*;
-use concordium_base::contracts_common::{Cursor, Deserial, ParseError};
-
-/// Attempt to parse the contract event into an event. This requires that the
-/// entire input is consumed if it is a known CIS2 event.
-impl<'a> TryFrom<&'a super::smart_contracts::ContractEvent> for Event {
-    type Error = ParseError;
-
-    fn try_from(value: &'a super::smart_contracts::ContractEvent) -> Result<Self, Self::Error> {
-        let data = value.as_ref();
-        let mut cursor = Cursor::new(data);
-        let res = Self::deserial(&mut cursor)?;
-        if cursor.offset == data.len() || matches!(&res, Self::Unknown) {
-            Ok(res)
-        } else {
-            Err(ParseError {})
-        }
-    }
-}
