7e69c741de5967b801f02608f8801bf6fc2ededb
Emil Lai <mail@emillai.com>
2023-08-28 09:41:36 +0200

    Doc.

--- src/v2/mod.rs
+++ src/v2/mod.rs
@@ -2186,6 +2186,12 @@ impl Client {
         })
     }
 
+    /// For a non-genesis block, this returns the [`QuorumCertificate`], a
+    /// [`TimeoutCertificate`] (if present) and [`EpochFinalizationEntry`] (if
+    /// present).
+    /// If the block being pointed to is *not* from protocol version 6 or
+    /// above, then [`InvalidArgument`](`tonic::Code::InvalidArgument`)
+    /// is returned.
     pub async fn get_block_certificates(
         &mut self,
         bi: impl IntoBlockIdentifier,
