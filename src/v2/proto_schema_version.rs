4f7ccbc1c450334dd2e77cb9f9d3d348401ea4b4
Emil Lai <mail@emillai.com>
2023-08-28 08:35:06 +0200

    format.

--- src/v2/proto_schema_version.rs
+++ src/v2/proto_schema_version.rs
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-pub const PROTO_SCHEMA_VERSION: &str = "bc0b3fcb12cbfb6336051a0bf40ae36474587790";
-=======
-pub const PROTO_SCHEMA_VERSION: &str = "e0712c295058bc3692fa3f32dde456f36c37cd90";
->>>>>>> main
+pub const PROTO_SCHEMA_VERSION: &str = "c079fde30b8b39d475f6dd8c049a9357bef0ab3c";
