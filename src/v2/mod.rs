5a77437577a22bc01e507b4e6662679f95f33418
rasmus-kirk <mail@rasmuskirk.com>
2024-03-12 14:37:57 +0100

    Addressed comments

--- src/v2/mod.rs
+++ src/v2/mod.rs
@@ -1155,7 +1155,7 @@ impl TryFrom<generated::NodeInfo> for types::NodeInfo {
         let version = semver::Version::parse(&node_info.peer_version)?;
         let local_time = chrono::DateTime::<chrono::Utc>::from(std::time::UNIX_EPOCH)
             + chrono::TimeDelta::try_milliseconds(node_info.local_time.require()?.value as i64)
-                .expect("Node local time out of bounds!");
+                .ok_or_else(|| anyhow::anyhow!("Node local time out of bounds!"))?;
         let uptime = types::DurationSeconds::from(node_info.peer_uptime.require()?.value).into();
         let network_info = node_info.network_info.require()?.try_into()?;
         let details = match node_info.details.require()? {
