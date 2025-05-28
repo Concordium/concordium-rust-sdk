841ae32ab9d573c450f3d21d908391fbea42ddfa
Allan Rasmussen <allanbrondum@gmail.com>
2025-05-28 11:34:04 +0200

    fmt

--- src/protocol_level_tokens/mod.rs
+++ src/protocol_level_tokens/mod.rs
@@ -57,9 +57,7 @@ impl TryFrom<generated::plt::TokenModuleRef> for TokenModuleRef {
 }
 
 impl From<generated::plt::CBor> for RawCbor {
-    fn from(wrapper: generated::plt::CBor) -> Self {
-        wrapper.value.into()
-    }
+    fn from(wrapper: generated::plt::CBor) -> Self { wrapper.value.into() }
 }
 
 impl TryFrom<generated::plt::TokenEvent> for TokenEvent {
@@ -68,7 +66,7 @@ impl TryFrom<generated::plt::TokenEvent> for TokenEvent {
     fn try_from(token_event: generated::plt::TokenEvent) -> Result<Self, Self::Error> {
         Ok(Self {
             token_id: token_event.token_id.require()?.try_into()?,
-            event: token_event.event.require()?.try_into()?,
+            event:    token_event.event.require()?.try_into()?,
         })
     }
 }
@@ -105,7 +103,7 @@ impl TryFrom<generated::plt::TokenModuleEvent> for TokenModuleEvent {
                 event.r#type,
             )
             .map_err(|err| tonic::Status::internal(err.to_string()))?,
-            details: event.details.require()?.into(),
+            details:    event.details.require()?.into(),
         })
     }
 }
@@ -115,10 +113,10 @@ impl TryFrom<generated::plt::TokenTransferEvent> for TokenTransferEvent {
 
     fn try_from(event: generated::plt::TokenTransferEvent) -> Result<Self, Self::Error> {
         Ok(Self {
-            from: event.from.require()?.try_into()?,
-            to: event.to.require()?.try_into()?,
+            from:   event.from.require()?.try_into()?,
+            to:     event.to.require()?.try_into()?,
             amount: event.amount.require()?.try_into()?,
-            memo: event
+            memo:   event
                 .memo
                 .map(concordium_base::transactions::Memo::try_from)
                 .transpose()?,
@@ -146,7 +144,7 @@ impl TryFrom<generated::plt::TokenHolder> for TokenHolder {
             HolderAddress::Account(account_address) => {
                 Ok(TokenHolder::HolderAccount(HolderAccount {
                     coin_info: None,
-                    address: account_address.try_into()?,
+                    address:   account_address.try_into()?,
                 }))
             }
         }
@@ -158,12 +156,12 @@ impl TryFrom<generated::plt::TokenModuleRejectReason> for TokenModuleRejectReaso
 
     fn try_from(value: generated::plt::TokenModuleRejectReason) -> Result<Self, Self::Error> {
         Ok(Self {
-            token_id: value.token_id.require()?.try_into()?,
+            token_id:    value.token_id.require()?.try_into()?,
             reason_type: protocol_level_tokens::TokenModuleCborTypeDiscriminator::try_from(
                 value.r#type,
             )
             .map_err(|err| tonic::Status::internal(err.to_string()))?,
-            details: value.details.map(|d| d.into()),
+            details:     value.details.map(|d| d.into()),
         })
     }
 }
