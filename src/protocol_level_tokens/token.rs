919f33a081643ea5083b116dcfeb33c581a34983
Abutalif <abutaliph@gmail.com>
2025-07-02 16:56:16 +0500

    validate transfer and gov oper

--- src/protocol_level_tokens/token.rs
+++ src/protocol_level_tokens/token.rs
@@ -1,5 +1,8 @@
 use concordium_base::{
-    common::{cbor::CborSerializationError, types::TransactionTime},
+    common::{
+        cbor::{CborSerializationError, CborSerialize},
+        types::TransactionTime,
+    },
     contracts_common::AccountAddress,
     hashes::TransactionHash,
     protocol_level_tokens::{operations, TokenAmount, TokenId, TokenTransfer},
@@ -47,6 +50,7 @@ impl Token {
     pub async fn transfer(
         &mut self,
         signer: &WalletAccount,
+        sender: AccountAddress,
         receiver: AccountAddress,
         amount: TokenAmount,
         expiry: Option<TransactionTime>,
@@ -65,7 +69,7 @@ impl Token {
 
         let operation = operations::transfer_tokens(receiver, amount);
 
-        let transaction = send::token_holder_operations(
+        let transaction = send::token_update_operations(
             &signer,
             signer.address,
             nonce,
@@ -98,7 +102,7 @@ impl Token {
 
         let operation = operations::mint_tokens(amount);
 
-        let transaction = send::token_governance_operations(
+        let transaction = send::token_update_operations(
             &signer,
             signer.address,
             nonce,
@@ -131,7 +135,7 @@ impl Token {
 
         let operation = operations::burn_tokens(amount);
 
-        let transaction = send::token_governance_operations(
+        let transaction = send::token_update_operations(
             &signer,
             signer.address,
             nonce,
@@ -164,7 +168,7 @@ impl Token {
 
         let operation = operations::add_token_allow_list(address);
 
-        let transaction = send::token_governance_operations(
+        let transaction = send::token_update_operations(
             &signer,
             signer.address,
             nonce,
@@ -197,7 +201,7 @@ impl Token {
 
         let operation = operations::remove_token_allow_list(address);
 
-        let transaction = send::token_governance_operations(
+        let transaction = send::token_update_operations(
             &signer,
             signer.address,
             nonce,
@@ -230,7 +234,7 @@ impl Token {
 
         let operation = operations::add_token_deny_list(address);
 
-        let transaction = send::token_governance_operations(
+        let transaction = send::token_update_operations(
             &signer,
             signer.address,
             nonce,
@@ -263,7 +267,7 @@ impl Token {
 
         let operation = operations::remove_token_deny_list(address);
 
-        let transaction = send::token_governance_operations(
+        let transaction = send::token_update_operations(
             &signer,
             signer.address,
             nonce,
@@ -298,16 +302,90 @@ impl Token {
         &mut self,
         sender: AccountAddress,
         payload: Vec<TokenTransfer>,
-    ) -> TokenResult<()> {
-        
-        Ok(())
+    ) -> TokenResult<bool> {
+        let decimals = self.info.token_state.decimals;
+
+        // Validate all amounts
+        if payload
+            .iter()
+            .any(|transfer| transfer.amount.decimals() != decimals)
+        {
+            return Ok(false);
+        }
+
+        // Check the sender ballance
+        let sender_balance = self
+            .balance_of(&sender.into(), None::<BlockIdentifier>)
+            .await?
+            .unwrap_or(TokenAmount::from_raw(0, decimals));
+
+        let payload_total = TokenAmount::from_raw(
+            payload.iter().fold(0u64, |acc, x| acc + x.amount.value()),
+            decimals,
+        );
+
+        if payload_total > sender_balance {
+            return Ok(false);
+        }
+
+        // Check if token has no allow and deny lists
+        let module_state = self.info.token_state.decode_module_state()?;
+        if !module_state.allow_list.is_none_or(|val| !val)
+            && !module_state.deny_list.is_none_or(|val| !val)
+        {
+            return Ok(true);
+        }
+
+        // Check sender and recievers for allow and deny lists
+        let mut addresses = Vec::with_capacity(payload.len() + 1);
+        addresses.push(sender.clone().into());
+
+        for p in payload {
+            let concordium_base::protocol_level_tokens::CborTokenHolder::Account(holder) =
+                p.recipient;
+            addresses.push(holder.address.into());
+        }
+
+        let accounts = futures::future::join_all(addresses.into_iter().map(|address| {
+            let mut client = self.client.clone();
+            async move {
+                client
+                    .get_account_info(&address, BlockIdentifier::LastFinal)
+                    .await
+            }
+        }))
+        .await;
+
+        for account in accounts {
+            let account_info = account?.response;
+            let token_state = match account_info
+                .tokens
+                .into_iter()
+                .find(|t| t.token_id == self.info.token_id)
+                .map(|t| t.state)
+            {
+                Some(state) => state,
+                None => return Ok(false),
+            };
+
+            let module_state = token_state.decode_module_state()?;
+            if module_state.allow_list.is_some_and(|val| !val)
+                || module_state.deny_list.is_some_and(|val| val)
+            {
+                return Ok(false);
+            }
+        }
+        Ok(true)
     }
 
-    // // done before the gov ops
-    // pub async fn validate_governance_operation(&self) -> QueryResult<()> { Ok(())
-    // }
+    pub async fn validate_governance_operation(&mut self, sender: AccountAddress) -> TokenResult<bool> {
+        let governance_account = match self.info.token_state.decode_module_state()?.governance_account {
+            concordium_base::protocol_level_tokens::CborTokenHolder::Account(holder) => holder.address,
+        };
+        Ok(governance_account == sender)
+    }
 
-    // pub async fn raw_operation(&self) -> QueryResult<()> {
-    //     Ok(())
-    // } // what is the use case?
+    pub async fn send_raw(&self, _operations: Vec<&impl CborSerialize>) -> TokenResult<()> { 
+        Ok(())
+    }
 }
